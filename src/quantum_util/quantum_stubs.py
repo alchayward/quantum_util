942602780f0aace94721485c8e0df13ae9d8d557
Andrew Hayward <alc.hayward@gmail.com>
2019-06-17 16:45:55 +0200

    updated some documentation

--- src/quantum_util/quantum_stubs.py
+++ src/quantum_util/quantum_stubs.py
@@ -4,7 +4,13 @@ from scipy import array, pi, cos, exp, sin, arctan2, sqrt, dot, conj, log, diag,
 from scipy.linalg import eigh, eigvalsh
 
 
-def ipr(v,n=4):
+def ipr(v, n=4):
+    """
+    Inverse participation ratio (Normalized)
+    :param v: vector of dimension D
+    :param n: ipr power. default 4
+    :return: ipr ( number between 1/D and 1.0)
+    """
     return sum(abs(v)**n)/sum(abs(v)**2)
 
 def expect(v1, op, v2):
@@ -13,6 +19,11 @@ def expect(v1, op, v2):
 def groundstate(h):
     return eigh(h)[1][:, 0]
 
-def com(v): 
+def com(v):
+    """
+    Normalized center of mass.
+    :param v: vector in configuration space
+    :return: com (scalar between -0.5 and 0.5)
+    """
     return np.dot(abs(v)**2, np.arange(len(v)))/len(v) - 1/2
 
